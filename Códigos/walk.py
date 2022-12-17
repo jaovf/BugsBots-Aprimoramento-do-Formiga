e403736fb1bf6bdab983c7be01bded0e5901e382
João Vítor Falcão <50123201+jaovf@users.noreply.github.com>
2022-12-17 18:05:51 -0300

    Update walk.py
    Adição de comentários ao código.

--- Códigos/walk.py
+++ Códigos/walk.py
@@ -1,3 +1,6 @@
+# Codigo de envio de mensagem por comunicacao serial da Colibri para as MBEDS
+
+# Importacao de bibliotecas
 import time
 import serial
 
@@ -5,16 +8,18 @@ while (1):
 
 	time.sleep(2)
 
+	# Porta serial de cada MBED
 	ser1 = serial.Serial("/dev/serial/by-id/usb-mbed_Microcontroller_101000000000000000000002F7F0C7A2-if01")
 
 	ser2 = serial.Serial("/dev/serial/by-id/usb-mbed_Microcontroller_101000000000000000000002F7F1EA94-if01")
 
-
+	# Envio da mensagem
 	ser1.write(1)
 	ser2.write(1)
 
 	time.sleep(10)
 
+	# Fechamento da conexao serial
 	ser1.close()
 	ser2.close()
 
